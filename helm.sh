3495a25578d51575ce2fb5ed6a1a18e31b935c58
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2024-06-17 01:05:45 +0200

    finish

--- helm.sh
+++ helm.sh
@@ -6,4 +6,6 @@ helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
   --set clusterName=atheon \
   --set serviceAccount.create=false \
-  --set serviceAccount.name=aws-load-balancer-controller
+  --set serviceAccount.name=aws-load-balancer-controller \
+  --set region=us-east-1 \
+  --set vpcId=vpc-09669e3299adac0c7
